c51c8042d560b2614cb2905814dc9f7c06101deb
YalcinKetenci <yalcinketencischool@gmail.com>
2021-02-18 19:50:08 +0200

    codes started to be written

--- src/test/java/com/translantik/pages/BasePage.java
+++ src/test/java/com/translantik/pages/BasePage.java
@@ -62,7 +62,11 @@ public abstract class  BasePage {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     public String getUserName(){
@@ -96,6 +100,8 @@ public abstract class  BasePage {
     public void navigateToModule(String tab, String module) {
         String tabLocator = "//span[normalize-space()='" + tab + "' and contains(@class, 'title title-level-1')]";
         String moduleLocator = "//span[normalize-space()='" + module + "' and contains(@class, 'title title-level-2')]";
+        new WebDriverWait(Driver.get(),20).until(ExpectedConditions.elementToBeClickable(Driver.get().findElement(By.xpath("//*[text()='Quick Launchpad']"))));
+        BrowserUtils.waitFor(1);
         try {
             BrowserUtils.waitForClickablility(By.xpath(tabLocator), 5);
             WebElement tabElement = Driver.get().findElement(By.xpath(tabLocator));
@@ -111,6 +117,7 @@ public abstract class  BasePage {
         } catch (Exception e) {
 //            BrowserUtils.waitForStaleElement(Driver.get().findElement(By.xpath(moduleLocator)));
             BrowserUtils.clickWithTimeOut(Driver.get().findElement(By.xpath(moduleLocator)),  5);
+            new WebDriverWait(Driver.get(),20).until(ExpectedConditions.elementToBeClickable(Driver.get().findElement(By.xpath("//*[text()='"+module+"']"))));
         }
     }
 
--- src/test/java/com/translantik/pages/VehicleOdometerPage.java
+++ src/test/java/com/translantik/pages/VehicleOdometerPage.java
@@ -1,4 +1,100 @@
 package com.translantik.pages;
 
-public class VehicleOdometerPage {
+import com.translantik.utilities.BrowserUtils;
+import com.translantik.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
+public class VehicleOdometerPage extends BasePage {
+
+    @FindBy(xpath = "(//*[@class='dropdown-menu'])[4]")
+    public WebElement gridSettingsWindow;
+
+    @FindBy(xpath = "//*[@class='ui-multiselect-menu ui-corner-all select-filter-widget dropdown-menu']")
+    public WebElement manageFiltersWindow;
+
+    @FindBy(xpath = "//*[@class='column-manager-search empty']/input")
+    public WebElement gridSettingsTextBox;
+
+    @FindBy(xpath = "//*[@class='ui-multiselect-menu ui-corner-all select-filter-widget dropdown-menu']/div/div/input")
+    public WebElement manageFiltersTextBox;
+
+    @FindBy(xpath = "(//table)[2]/tbody/tr/td[1]/label")
+    public List<WebElement> namesOfTheDataOnTheGridSettings;
+
+    @FindBy(xpath = "//*[text()='Select All']")
+    public WebElement selectAll;
+
+    @FindBy(xpath = "//*[@class='column-filter-renderable-tab active']")
+    public WebElement All;
+
+    @FindBy(xpath = "//*[@class='column-filter-renderable-tab']")
+    public WebElement Selected;
+
+
+
+
+
+
+
+
+
+    public WebElement clickTheButton(String buttonTitle){
+        waitUntilLoaderScreenDisappear();
+        Driver.get().findElement(By.xpath("//*[@title='"+ buttonTitle +"']")).click();
+
+        return Driver.get().findElement(By.xpath("//*[@title='"+ buttonTitle +"']"));
+        }
+
+    public WebElement getWindow(String windowName){
+
+        switch (windowName.toLowerCase()){
+            case "grid settings":
+                gridSettingsWindow.click();
+                return gridSettingsWindow;
+            case "manage filters":
+                manageFiltersWindow.click();
+                return manageFiltersWindow;
+            default:
+                System.out.println("Invalid window name to get window !!!");
+                return null;
+        }
+    }
+
+    public WebElement getTextBox(String windowName){
+        switch (windowName.toLowerCase()){
+            case "grid settings":
+                return gridSettingsTextBox;
+            case "manage filters":
+                return manageFiltersTextBox;
+            default:
+                System.out.println("Invalid window name to get window text-box !!!");
+                return null;
+        }
+    }
+
+    public WebElement findElementWithExactText(String exactText){
+        return Driver.get().findElement(By.xpath("//*[text()='"+exactText+"']"));
+    }
+
+    public WebElement getGridSettingsButton(String buttonName){
+        switch (buttonName.toLowerCase()){
+            case "selected":
+                return Selected;
+            case "all":
+                return All;
+            case "select all":
+                return selectAll;
+            default:
+                System.out.println("Invalid button name");
+                return null;
+        }
+    }
 }
+
