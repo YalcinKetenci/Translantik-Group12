64198f14a6b3236408d21b1d303ca62f54ae37aa
YalcinKetenci <yalcinketencischool@gmail.com>
2021-02-18 11:23:52 +0200

    DashboardPage added Double quotes repaired

--- src/test/java/com/translantik/step_definitions/TRAN8_FilteringFunctionsStepDef.java
+++ src/test/java/com/translantik/step_definitions/TRAN8_FilteringFunctionsStepDef.java
@@ -5,26 +5,26 @@ import io.cucumber.java.en.When;
 
 public class TRAN8_FilteringFunctionsStepDef {
 
-    @When("the user navigates to “Fleet” “Vehicle Odometer”")
-    public void the_user_navigates_to_Fleet_Vehicle_Odometer() {
+    @When("the user navigates to {string} {string}")
+    public void the_user_navigates_to(String string, String string2) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
 
-    @Then("the user should be able to click “Grid Settings” button")
-    public void the_user_should_be_able_to_click_Grid_Settings_button() {
+    @Then("the user should be able to click {string} button")
+    public void the_user_should_be_able_to_click_button(String string) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
 
-    @Then("the user should be able to see “Grid Settings” window")
-    public void the_user_should_be_able_to_see_Grid_Settings_window() {
+    @Then("the user should be able to see {string} window")
+    public void the_user_should_be_able_to_see_window(String string) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
 
-    @Then("the user should be able to enters one of the given data inside the text-box on the “Grid Settings” window")
-    public void the_user_should_be_able_to_enters_one_of_the_given_data_inside_the_text_box_on_the_Grid_Settings_window(io.cucumber.datatable.DataTable dataTable) {
+    @Then("the user should be able to enters one of the given data inside the text-box on the {string} window")
+    public void the_user_should_be_able_to_enters_one_of_the_given_data_inside_the_text_box_on_the_window(String string, io.cucumber.datatable.DataTable dataTable) {
         // Write code here that turns the phrase above into concrete actions
         // For automatic transformation, change DataTable to one of
         // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
@@ -35,14 +35,14 @@ public class TRAN8_FilteringFunctionsStepDef {
         throw new io.cucumber.java.PendingException();
     }
 
-    @Then("the user should be able to see the only  options that includes the written character on the “Grid Settings” window")
-    public void the_user_should_be_able_to_see_the_only_options_that_includes_the_written_character_on_the_Grid_Settings_window() {
+    @Then("the user should be able to see the only  options that includes the written character on the {string} window")
+    public void the_user_should_be_able_to_see_the_only_options_that_includes_the_written_character_on_the_window(String string) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
 
-    @Then("the user should be able to enters “car” inside the “Quick Search” area on the “Grid Settings” window")
-    public void the_user_should_be_able_to_enters_car_inside_the_Quick_Search_area_on_the_Grid_Settings_window() {
+    @Then("the user should be able to enters {string} inside the {string} area on the {string} window")
+    public void the_user_should_be_able_to_enters_inside_the_area_on_the_window(String string, String string2, String string3) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
@@ -53,8 +53,8 @@ public class TRAN8_FilteringFunctionsStepDef {
         throw new io.cucumber.java.PendingException();
     }
 
-    @Then("the user should NOT be able to see any given options on the “Grid Settings” window")
-    public void the_user_should_NOT_be_able_to_see_any_given_options_on_the_Grid_Settings_window(io.cucumber.datatable.DataTable dataTable) {
+    @Then("the user should NOT be able to see any given options on the {string} window")
+    public void the_user_should_NOT_be_able_to_see_any_given_options_on_the_window(String string, io.cucumber.datatable.DataTable dataTable) {
         // Write code here that turns the phrase above into concrete actions
         // For automatic transformation, change DataTable to one of
         // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
@@ -65,8 +65,8 @@ public class TRAN8_FilteringFunctionsStepDef {
         throw new io.cucumber.java.PendingException();
     }
 
-    @Then("the user should be able to click “Select all” button on the “Grid Settings” window")
-    public void the_user_should_be_able_to_click_Select_all_button_on_the_Grid_Settings_window() {
+    @Then("the user should be able to click {string} button on the {string} window")
+    public void the_user_should_be_able_to_click_button_on_the_window(String string, String string2) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
@@ -89,20 +89,14 @@ public class TRAN8_FilteringFunctionsStepDef {
         throw new io.cucumber.java.PendingException();
     }
 
-    @Then("the user should be able to click “Selected” button on the “Grid Settings” window")
-    public void the_user_should_be_able_to_click_Selected_button_on_the_Grid_Settings_window() {
+    @Then("the user should be able to see the only selected options on the {string} window")
+    public void the_user_should_be_able_to_see_the_only_selected_options_on_the_window(String string) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
 
-    @Then("the user should be able to see the only selected options on the “Grid Settings” window")
-    public void the_user_should_be_able_to_see_the_only_selected_options_on_the_Grid_Settings_window() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @Then("the user should NOT be able to see the all given options on the “Grid Settings” window")
-    public void the_user_should_NOT_be_able_to_see_the_all_given_options_on_the_Grid_Settings_window(io.cucumber.datatable.DataTable dataTable) {
+    @Then("the user should NOT be able to see the all given options on the {string} window")
+    public void the_user_should_NOT_be_able_to_see_the_all_given_options_on_the_window(String string, io.cucumber.datatable.DataTable dataTable) {
         // Write code here that turns the phrase above into concrete actions
         // For automatic transformation, change DataTable to one of
         // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
@@ -113,14 +107,8 @@ public class TRAN8_FilteringFunctionsStepDef {
         throw new io.cucumber.java.PendingException();
     }
 
-    @Then("the user should be able to click “All” button on the “Grid Settings” window")
-    public void the_user_should_be_able_to_click_All_button_on_the_Grid_Settings_window() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @Then("the user should be able to see the all given options on the “Grid Settings” window")
-    public void the_user_should_be_able_to_see_the_all_given_options_on_the_Grid_Settings_window(io.cucumber.datatable.DataTable dataTable) {
+    @Then("the user should be able to see the all given options on the {string} window")
+    public void the_user_should_be_able_to_see_the_all_given_options_on_the_window(String string, io.cucumber.datatable.DataTable dataTable) {
         // Write code here that turns the phrase above into concrete actions
         // For automatic transformation, change DataTable to one of
         // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
@@ -143,20 +131,8 @@ public class TRAN8_FilteringFunctionsStepDef {
         throw new io.cucumber.java.PendingException();
     }
 
-    @Then("the user should be able to click “x” button on the “Grid Settings” window")
-    public void the_user_should_be_able_to_click_x_button_on_the_Grid_Settings_window() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
     @Then("Grid Settings window should be disappear")
     public void grid_Settings_window_should_be_disappear() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @Then("the user should be able to click “Filters” button")
-    public void the_user_should_be_able_to_click_Filters_button() {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
@@ -167,51 +143,9 @@ public class TRAN8_FilteringFunctionsStepDef {
         throw new io.cucumber.java.PendingException();
     }
 
-    @Then("the user should be able to click “Manage Filters” button")
-    public void the_user_should_be_able_to_click_Manage_Filters_button() {
+    @Then("the user should be able to enters {string} inside the text-box on the {string} window")
+    public void the_user_should_be_able_to_enters_inside_the_text_box_on_the_window(String string, String string2) {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @Then("the user should be able to see “Manage Filters” window")
-    public void the_user_should_be_able_to_see_Manage_Filters_window() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @Then("the user should be able to enters one of the given data inside the text-box on the “Manage Filters” window")
-    public void the_user_should_be_able_to_enters_one_of_the_given_data_inside_the_text_box_on_the_Manage_Filters_window(io.cucumber.datatable.DataTable dataTable) {
-        // Write code here that turns the phrase above into concrete actions
-        // For automatic transformation, change DataTable to one of
-        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
-        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
-        // Double, Byte, Short, Long, BigInteger or BigDecimal.
-        //
-        // For other transformations you can register a DataTableType.
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @Then("the user should be able to see the only  options that includes the written character on the “Manage Filters” window")
-    public void the_user_should_be_able_to_see_the_only_options_that_includes_the_written_character_on_the_Manage_Filters_window() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @Then("the user should be able to enters “car” inside the text-box on the “Manage Filters” window")
-    public void the_user_should_be_able_to_enters_car_inside_the_text_box_on_the_Manage_Filters_window() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @Then("the user should NOT be able to see any given options on the “Manage Filters” window")
-    public void the_user_should_NOT_be_able_to_see_any_given_options_on_the_Manage_Filters_window(io.cucumber.datatable.DataTable dataTable) {
-        // Write code here that turns the phrase above into concrete actions
-        // For automatic transformation, change DataTable to one of
-        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
-        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
-        // Double, Byte, Short, Long, BigInteger or BigDecimal.
-        //
-        // For other transformations you can register a DataTableType.
         throw new io.cucumber.java.PendingException();
     }
 
@@ -251,20 +185,14 @@ public class TRAN8_FilteringFunctionsStepDef {
         throw new io.cucumber.java.PendingException();
     }
 
-    @Then("the user should be able to click the “Odometer Value” option")
-    public void the_user_should_be_able_to_click_the_Odometer_Value_option() {
+    @Then("the user should be able to click the {string} option")
+    public void the_user_should_be_able_to_click_the_option(String string) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
 
-    @Then("the user should be able to click the “Equals” option")
-    public void the_user_should_be_able_to_click_the_Equals_option() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @Then("the user should be able to put “{int}”")
-    public void the_user_should_be_able_to_put(Integer int1) {
+    @Then("the user should be able to put {string}")
+    public void the_user_should_be_able_to_put(String string) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
@@ -275,26 +203,20 @@ public class TRAN8_FilteringFunctionsStepDef {
         throw new io.cucumber.java.PendingException();
     }
 
-    @Then("the user should be able to see only include “{int}” odometers")
-    public void the_user_should_be_able_to_see_only_include_odometers(Integer int1) {
+    @Then("the user should be able to see only include {string} odometers")
+    public void the_user_should_be_able_to_see_only_include_odometers(String string) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
 
-    @Then("the user should NOT be able to see only include “{int}” odometers")
-    public void the_user_should_NOT_be_able_to_see_only_include_odometers(Integer int1) {
+    @Then("the user should NOT be able to see only include {string} odometers")
+    public void the_user_should_NOT_be_able_to_see_only_include_odometers(String string) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
 
-    @Then("the user should be able to select “Id” option")
-    public void the_user_should_be_able_to_select_Id_option() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @Then("the user should be able to click “Refresh” button")
-    public void the_user_should_be_able_to_click_Refresh_button() {
+    @Then("the user should be able to select {string} option")
+    public void the_user_should_be_able_to_select_option(String string) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
@@ -304,5 +226,6 @@ public class TRAN8_FilteringFunctionsStepDef {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
+
 
 }
