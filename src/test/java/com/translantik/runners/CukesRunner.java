7190fdab85fa6fe696679108c3c3b83125493bea
YalcinKetenci <yalcinketencischool@gmail.com>
2021-02-23 23:14:37 +0200

    continued writing code

--- src/test/java/com/translantik/runners/CukesRunner.java
+++ src/test/java/com/translantik/runners/CukesRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/translantik/step_definitions",
         dryRun = false,
-        tags = "@TRAN-254"
+        tags = "@TRAN-249"
 )
 public class CukesRunner {
 
