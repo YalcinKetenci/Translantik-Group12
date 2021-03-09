aa3914db702b93d4d45e967c3079b69eab806a08
Arif Şükrü Özdil <65284314+Arifsukru@users.noreply.github.com>
2021-03-09 11:53:42 +0100

    US-29 features and step definitions added

--- src/test/java/com/translantik/step_definitions/LoginStepDef.java
+++ src/test/java/com/translantik/step_definitions/LoginStepDef.java
@@ -21,6 +21,16 @@ public class LoginStepDef {
         new LoginPage().loginAsATruckDriver();
     }
 
+    @When("The user enters valid credentials for sales manager")
+    public void the_user_enters_valid_credentials_for_sales_manager() {
+        new LoginPage().loginAsASalesManager();
+    }
+
+    @When("The user enters valid credentials for store manager")
+    public void the_user_enters_valid_credentials_for_store_manager() {
+        new LoginPage().loginAsAStoreManager();
+    }
+
     @Then("User should be able to login")
     public void user_should_be_able_to_login() {
         String currentTitle = "Login";
---
+++ src/test/java/com/translantik/step_definitions/US_29_SalesManagerStoreManagerWidgetPinFavStepDefs.java
@@ -0,0 +1,66 @@
+package com.translantik.step_definitions;
+
+import com.translantik.pages.Dashboard;
+import com.translantik.utilities.Driver;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+
+public class US_29_SalesManagerStoreManagerWidgetPinFavStepDefs {
+
+
+    @When("the user clicks plus button")
+    public void the_user_clicks_plus_button() {
+        new Dashboard().waitUntilLoaderScreenDisappear();
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Driver.get().findElement(By.xpath("//button[@data-role='sidebar-add-widget']")).click();
+    }
+
+    @When("sidebar widgets page opened")
+    public void sidebar_widgets_page_opened() {
+        System.out.println("DONE");
+    }
+
+    @When("the user click add button")
+    public void the_user_click_add_button() throws InterruptedException {
+        Thread.sleep(1000);
+        Driver.get().findElement(By.xpath("//a[@class='btn widget-picker-add-btn btn-primary']")).click();
+    }
+
+    @Then("the widget is added")
+    public void the_widget_is_added() throws InterruptedException {
+        Thread.sleep(1000);
+        Assert.assertTrue(Driver.get().findElement(By.xpath("//a[@class='btn widget-picker-add-btn btn-primary']")).isDisplayed());
+    }
+
+    @When("the user click star button")
+    public void the_user_click_star_button() throws InterruptedException {
+        Thread.sleep(10000);
+        Driver.get().findElement(By.xpath("//i[@class='fa-star hide-text']")).click();
+    }
+
+    @Then("the user made the page favorite")
+    public void the_user_made_the_page_favorite() throws InterruptedException {
+        Thread.sleep(1000);
+        Assert.assertTrue(Driver.get().findElement(By.xpath("//button[@class='btn favorite-button gold-icon']")).isDisplayed());
+
+    }
+
+    @When("the user click pin button")
+    public void the_user_click_pin_button() throws InterruptedException {
+        Thread.sleep(10000);
+        Driver.get().findElement(By.xpath("//button[@title='Pin/unpin the page']")).click();
+    }
+
+    @Then("the user made the page pinned")
+    public void the_user_made_the_page_pinned() throws InterruptedException {
+        Thread.sleep(1000);
+        Assert.assertTrue(Driver.get().findElement(By.xpath("//button[@class='btn minimize-button gold-icon']")).isDisplayed());
+    }
+
+}
